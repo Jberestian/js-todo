fb99a024fc1ccba442486b18e20c79f6e330e33e
Jberestian <31864132+Jberestian@users.noreply.github.com>
2022-07-14 10:18:19 +0000

    Deploying to gh-pages from @ Jberestian/js-todo@e8effe8ac8c7be36a91e60010ae039146772612c 🚀

---
+++ index.2988b761.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".js-form");var e=document.querySelector(".js-input"),t=document.querySelector(".js-output");e.addEventListener("input",(function(e){t.textContent=e.currentTarget.value}))}();
+//# sourceMappingURL=index.2988b761.js.map
---
+++ index.32994712.js
@@ -0,0 +1,2 @@
+document.querySelector(".js-form");const e=document.querySelector(".js-input"),t=document.querySelector(".js-output");e.addEventListener("input",(e=>{t.textContent=e.currentTarget.value}));
+//# sourceMappingURL=index.32994712.js.map
